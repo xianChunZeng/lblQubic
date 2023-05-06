496abd20e2132cd10ef63cb9397b30143f686955
jpmarceaux <johnpaulmarceaux@gmail.com>
2023-05-05 22:57:34 -0700

    TimeRabi class based on standard AbstractCalibrationExperiment

--- chipcalibration/rabi_experiments.py
+++ chipcalibration/rabi_experiments.py
@@ -4,6 +4,74 @@ import numpy as np
 from chipcalibration.abstract_calibration import AbstractCalibrationExperiment
 from qubic.state_disc import GMMManager
 import warnings
+from collections import OrderedDict
+
+class GMMRabi(AbstractCalibrationExperiment):
+    """
+    Time Rabi experiments to construct GMM Managers for the register
+    """
+    def __init__(self, target_qubits, target_amplitude, pulse_width_interval,
+                 chanmap_or_channel_configs):
+        self.target_register = target_qubits
+        self.gmm_manager = GMMManager(chanmap_or_chan_cfgs=chanmap_or_channel_configs)
+
+        self.drive_amplitude = target_amplitude
+        self.pulse_widths = pulse_width_interval
+
+        self.circuits = self._make_circuits()
+
+    def run_and_report(self, jobmanager, num_shots_per_circuit, qchip, plotting=True):
+        """
+        run the time Rabi experiments and make a GMM Manager
+        """
+        data = self._collect_data(jobmanager, num_shots_per_circuit, qchip)
+        self.gmm_manager = self._fit_gmmm(data)
+        self.shots = self.gmm_manager.predict(data)
+
+        if plotting:
+            fig, axs = plt.subplots(len(self.target_register))
+            if len(self.readout_register) == 1:
+                axs = [axs]
+            for idx, qid in enumerate(self.target_register):
+                axs[idx].scatter(data[qid].real, data[qid].imag, c=self.shots[qid], cmap='viridis')
+            plt.tight_layout()
+            plt.show()
+
+    def _fit_data(self, data, fit_routine=None, prior_estimates=None):
+        pass
+
+    def _fit_gmmm(self, data):
+        gmmm = GMMManager(chanmap_or_chan_cfgs=self.chanmap_or_channel_configs)
+        all_collected_IQ_shots = dict()
+        for qid in self.target_register:
+            all_collected_IQ_shots.update(data[qid])
+        gmmm.fit(all_collected_IQ_shots)
+        return gmmm
+
+    def _make_circuits(self):
+        """
+        Make list of circuits used for rabi measurement. and the list of pulse width. So there will be a total of
+        1 circuits, each of which contains len(pulse_widths) measurements. A 400 us
+        delay is inserted between each measurement.
+        """
+        circuits = OrderedDict()
+        for qid in self.target_register:
+            for twidth in self.pulse_widths:
+                cur_circ = []
+                cur_circ.append({'name': 'delay', 't': 400.e-6})
+                cur_circ.append({'name': 'rabi', 'qubit': self.target_register,
+                                 'modi': {(0, 'twidth'): twidth}, (0, 'amp'): self.drive_amplitude})
+                cur_circ.append({'name': 'barrier', 'qubit': self.readout_register})
+                cur_circ.append({'name': 'read', 'qubit': [qid]})
+                circuits[qid] = cur_circ
+        return circuits
+
+    def _collect_data(self, jobmanager, num_shots_per_circuit, qchip):
+        """
+        runs the circuits using the jabmanager
+        returns raw IQ shots
+        """
+        return jobmanager.collect_raw_IQ(self.circuits, num_shots_per_circuit, qchip)
 
 class TimeRabi(AbstractCalibrationExperiment):
     """
@@ -22,6 +90,7 @@ class TimeRabi(AbstractCalibrationExperiment):
         self.readout_register = readout_register
         self.chanmap_or_channel_configs = chanmap_or_channel_configs
         self.gmm_manager = None
+        self.drive_amplitude = target_amplitude
 
         self.pulse_widths = pulse_width_interval
 
@@ -76,7 +145,8 @@ class TimeRabi(AbstractCalibrationExperiment):
         for twidth in self.pulse_widths:
             cur_circ = []
             cur_circ.append({'name': 'delay', 't': 400.e-6})
-            cur_circ.append({'name': 'rabi', 'qubit': self.target_register, 'modi': {(0, 'twidth'): twidth}})
+            cur_circ.append({'name': 'rabi', 'qubit': self.target_register,
+                             'modi': {(0, 'twidth'): twidth}, (0, 'amp'): self.drive_amplitude})
             cur_circ.append({'name': 'barrier', 'qubit': self.readout_register})
             for qid in self.readout_register:
                 cur_circ.append({'name': 'read', 'qubit': [qid]})
